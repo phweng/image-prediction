0adf7a16c29d7b2188ccd71402d55d63243619c9
Pin-Hung Weng <phweng@Pin-Hungs-MacBook-Air.local>
2021-05-25 21:44:30 +0200

    adding image pediction project file

---
+++ brain.py
@@ -0,0 +1,15 @@
+from imageai.Prediction import ImagePrediction
+import os
+execution_path = os.getcwd()
+
+prediction = ImagePrediction()
+prediction.setModelTypeAsMobileNetV2()
+prediction.setModelPath(os.path.join(execution_path, "mobilenet_v2.h5"))
+prediction.loadModel()
+
+
+predictions, probabilities = prediction.classifyImage(os.path.join(execution_path, "giraffe.jpg"), result_count=5 )
+for eachPrediction, eachProbability in zip(predictions, probabilities):
+    print(eachPrediction , " : " , eachProbability)
+
+
